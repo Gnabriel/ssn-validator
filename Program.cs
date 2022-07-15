61c2b7443412e956625436fbfb0515d55f846b22
Gabriel Ellebrink <code.ngat4@simplelogin.co>
2022-07-15 11:25:57 +0200

    Add dummy classes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
 namespace ssn_validator
 {
+  // Enum that represents genders.
+  enum Gender
+  {
+    MALE,
+    FEMALE
+  }
+
+  // Class that represents one person containing its full name and SSN.
+  class Person
+  {
+    string firstName;
+    string lastName;
+    int ssn;
+
+    public Person (string firstName, string lastName, int ssn)
+    {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.ssn = ssn;
+    }
+
+    public bool validateSSN ()
+    {
+      return true;
+    }
+
+    public Gender checkGender ()
+    {
+      return Gender.MALE;
+    }
+  }
+
   internal static class Program
   {
     /// <summary>
